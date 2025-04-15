2d0199cc02c7bfb99362863735394ab4a5203e67
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-04-15 19:56:17 -0300

    Adicionando um h3 no html, e uma variavel no js para mostrar ao usuario quantas vezes ele tentou.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let numeroSorteado;
+let tentativas = 0
 
     function sortearNumero() {
       numeroSorteado = Math.floor(Math.random() * 500) + 1;
       console.log("Número sorteado:", numeroSorteado);
       alert('Número sorteado! Agora tente adivinhar.');
-      // Opcional: Você pode reabilitar o botão de tentar aqui, se o tiver desabilitado após o acerto.
+      tentativas = 0
+      const tentar = document.getElementById('tentar');
+
+      if (tentar) {
+      tentar.innerHTML = '';
+      }
     }
 
     function clicou() {
       const numer = document.getElementById('num').value;
       const principal = document.getElementById('princ');
+      const tentar = document.getElementById('tentar');
 
       if (numeroSorteado === undefined) {
         alert('O número ainda não foi sorteado. Clique no botão "Sortear Número" primeiro.');
@@ -21,9 +28,19 @@ let numeroSorteado;
         
       } else if (parseInt(numer) === numeroSorteado) {
         alert('Parabéns! Você acertou!');
-      } else if (parseInt(numer) > numeroSorteado) {
+
+        tentativas++;
+
+        if (tentar) {
+          tentar.innerHTML = `Parabéns, você precisou de ${tentativas} tentativa(s) para acertar o número.`;
+        }
+      } 
+      else if (parseInt(numer) > numeroSorteado) {
+        tentativas++;
         alert('O número é menor.');
-      } else {
+      } 
+      else {
+        tentativas++;
         alert('O número é maior.');
       }
     }
