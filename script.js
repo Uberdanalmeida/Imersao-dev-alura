0fd32c11e5fb0f23782f25e3a511d0f431827fbf
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-04-16 11:31:10 -0300

    Adicionando imagem de um troféu ao projeto, a imagem aparessera apenas quando o usuario acertar o numero sorteado.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ let tentativas = 0
       const numer = document.getElementById('num').value;
       const principal = document.getElementById('princ');
       const tentar = document.getElementById('tentar');
+      const img = document.getElementById('foto')
 
       if (numeroSorteado === undefined) {
         alert('O número ainda não foi sorteado. Clique no botão "Sortear Número" primeiro.');
@@ -27,12 +28,13 @@ let tentativas = 0
         alert('Escolha um número válido entre 1 e 500.');
         
       } else if (parseInt(numer) === numeroSorteado) {
+        img.src = 'imagem/trofeu.png'
         alert('Parabéns! Você acertou!');
 
         tentativas++;
 
         if (tentar) {
-          tentar.innerHTML = `Parabéns, você precisou de ${tentativas} tentativa(s) para acertar o número.`;
+          tentar.innerHTML = `<p id='par'>Parabéns você acertou.<br/> Você <span>precisou de ${tentativas} tentativa(s)</span> para acertar o número.<p/>`;
         }
       } 
       else if (parseInt(numer) > numeroSorteado) {
